3c2a04eb05864679df1363250f4b692614e24816
IRINA <irina.ulida@yandex.ru>
2019-05-22 11:57:32 +0300

    Home work №1 (day,table)

---
+++ home/home 1/home 1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number day of week");
+            string selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "1":
+                    Console.WriteLine("Monday");
+                    break;
+                case "2":
+                    Console.WriteLine("Tuesday");
+                    break;
+                case "3":
+                    Console.WriteLine("Wednesday");
+                    break;
+                case "4":
+                    Console.WriteLine("Thursday");
+                    break;
+                case "5":
+                    Console.WriteLine("Friday");
+                    break;
+                case "6":
+                    Console.WriteLine("Saturday");
+                    break;
+                case "7":
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("You pressed incorrect void");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ home/home 2/home 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number fгом the multiplicattion table");
+
+            string number = Console.ReadLine();
+            int j = Convert.ToInt32(number);
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"{j}x{i} = {j * i}");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
+
+
